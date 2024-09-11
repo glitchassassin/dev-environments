d34e14832c4901fa5e3ff908866f964e0beb869b
Jon <jon.winsley@gmail.com>
2024-09-11 17:28:44 -0400

    fixing lazygit

--- configure.sh
+++ configure.sh
@@ -17,6 +17,7 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 install lazygit /usr/local/bin
+touch /root/.config/lazygit/config.yml
 
 # Install ripgrep
 RIPGREP_VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep -Po '"tag_name": "\K[^"]*')
